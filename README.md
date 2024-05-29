ae0a0acbb215bff35d03b3975c7434c3b9bef7ca
Luis Miguel Cardenas <lcardenasl@grupo-exito.com>
2024-05-29 16:49:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# widges_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/widges_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.widges_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
